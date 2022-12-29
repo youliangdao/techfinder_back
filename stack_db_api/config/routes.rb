92c3a0fa00a58f0fcfd9f77d0de2897b3f84df6d
youliangdao <youliangdaojing5@gmail.com>
2022-12-29 18:56:12 +0900

    ✨Feat: 認証用のエンドポイントを設定

--- stack_db_api/config/routes.rb
+++ stack_db_api/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root  'rails/welcome#index'
-  namespace :api do
+  namespace :api, format: "json" do
     namespace :v1 do
       # api test action
       resources :hello, only:[:index]
+      resource :authentication, only: %w[create]
     end
   end  
 end

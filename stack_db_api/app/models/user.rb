6b4032850dce14db13dd3ade2ac17e45ef34c0e1
youliangdao <youliangdaojing5@gmail.com>
2022-12-29 18:53:23 +0900

    🔨Refactor: Userモデルのバリデーションを一部修正

--- stack_db_api/app/models/user.rb
+++ stack_db_api/app/models/user.rb
@@ -8,13 +8,24 @@
 #  github_username  :string
 #  nickname         :string(40)       not null
 #  twitter_username :string
-#  uid              :string
+#  uid              :string           not null
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
+# Indexes
+#
+#  index_users_on_uid  (uid) UNIQUE
+#
 class User < ApplicationRecord
   validates :nickname,    presence: true, length: { maximum: 40 }
-  validates :uid,         uniqueness: true
-  validates :avatar_key,  uniqueness: true
+  validates :uid,         presence: true, uniqueness: true
   validates :description, length: { maximum: 160 } 
+
+  def self.find_or_create_user(user_info)
+
+    user = User.find_by(uid: user_info[:uid])
+    return user if user
+
+    User.create!(uid: user_info[:uid], nickname: "User_#{user_info[:uid]}")
+  end  
 end

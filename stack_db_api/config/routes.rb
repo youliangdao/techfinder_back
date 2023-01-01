e2c0604c013b3d5181773771574291f027dd34a9
youliangdao <youliangdaojing5@gmail.com>
2023-01-01 12:31:00 +0900

    ✨Feat: プロフィール情報と署名付きURLの発行に関するエンドポイントを定める

--- stack_db_api/config/routes.rb
+++ stack_db_api/config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   namespace :api, format: "json" do
     namespace :v1 do
       # api test action
-      resources :hello, only:[:index]
+      resources :hello, only: %w[index]
+      resource :image, only: %w[create show]
       resource :authentication, only: %w[create]
+      resource :profile, only: %w[show update]
     end
   end  
 end

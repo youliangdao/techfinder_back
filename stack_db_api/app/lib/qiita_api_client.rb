2d46b79c3c6bc8af9c0cca014b38a995b306d221
youliangdao <youliangdaojing5@gmail.com>
2023-01-03 09:28:23 +0900

    ✨Feat: Faradayを利用してQiitaAPI連携クラスを作成

---
+++ stack_db_api/app/lib/qiita_api_client.rb
@@ -0,0 +1,31 @@
+class QiitaApiClient
+  class HTTPError < StandardError
+    def initialize(response)
+      super "status=#{response.status} body=#{response.body}"
+    end
+  end  
+  class << self
+    def connection
+      Faraday::Connection.new('https://qiita.com') do |builder|
+        builder.authorization :Bearer, "#{ENV['QIITA_TOKEN']}"
+        builder.request :url_encoded
+        builder.response :logger
+        builder.adapter Faraday.default_adapter
+        builder.response :json, :content_type => 'application/json'
+      end
+    end
+
+    def get_items(page)
+      response = connection.get('/api/v2/items') do |req|
+        req.params[:per_page] = 100
+        req.params[:page] = page
+        req.params[:query] = "title:個人開発 stocks:>50 created:>2020-01-01 OR title:ポートフォリオ stocks:>50 created:>2020-01-01 OR tag:個人開発 stocks:>50 created:>2020-01-01 OR tag:ポートフォリオ stocks:>50 created:>2020-01-01"
+      end
+      if response.success?
+        response.body
+      else
+        raise QiitaApiClient::HTTPError.new(response)
+      end
+    end
+  end
+end

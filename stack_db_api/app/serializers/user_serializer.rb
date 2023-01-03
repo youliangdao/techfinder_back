89e2099f8aa174f03729d42c304b59df3c712fbe
youliangdao <youliangdaojing5@gmail.com>
2023-01-03 09:30:38 +0900

    📚Doc: serializerにカラム情報を追記

--- stack_db_api/app/serializers/user_serializer.rb
+++ stack_db_api/app/serializers/user_serializer.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  avatar_key       :string
+#  description      :text
+#  github_username  :string
+#  nickname         :string(40)       not null
+#  twitter_username :string
+#  uid              :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_uid  (uid) UNIQUE
+#
 class UserSerializer
   include JSONAPI::Serializer
   attributes :nickname, :description, :avatar_key

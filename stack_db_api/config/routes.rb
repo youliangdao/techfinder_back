9256c43dffe02a5656ac8a5d4f2b655dbdc6b2eb
youliangdao <youliangdaojing5@gmail.com>
2023-01-04 17:38:20 +0900

    ✨Feat: 記事とカテゴリ、カテゴリに紐づく記事データにアクセスするためのエンドポイントを設定

--- stack_db_api/config/routes.rb
+++ stack_db_api/config/routes.rb
@@ -7,6 +7,20 @@ Rails.application.routes.draw do
       resource :image, only: %w[create show]
       resource :authentication, only: %w[create]
       resource :profile, only: %w[show update]
+      resources :articles, only: %w[index] do
+        collection do
+          get 'rising', to: 'articles#rising'
+        end
+      end
+      scope '/:category_name' do
+        get 'articles', to: 'category/articles#index'
+      end
+      resources :categories, only: %w[index] do
+        collection do
+          get 'popular', to: 'categories#popular'
+        end
+      end
+      get 'categories/:category_name', to: 'categories#show'
     end
   end  
 end

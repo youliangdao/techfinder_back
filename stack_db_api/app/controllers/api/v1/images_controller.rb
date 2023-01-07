2fcd0b3620ebc43ed53a55e4141ddd7c2d314041
youliangdao <youliangdaojing5@gmail.com>
2023-01-07 17:53:12 +0900

    ✨Feat: ユーザーのアバター画像を取得する処理を実装+署名付きURLを取得するメソッドを切り出す

--- stack_db_api/app/controllers/api/v1/images_controller.rb
+++ stack_db_api/app/controllers/api/v1/images_controller.rb
@@ -1,18 +1,29 @@
 class Api::V1::ImagesController < Api::V1::BaseController
+  skip_before_action :authenticate, only: %w[avatar]
   def create
     s3_direct_post
     render json: { status: :ok, url: @resource.url, fields: @resource.fields }
   end
 
   def show
-    url = Aws::S3::Object.new(ENV['S3_BUCKET_NAME'], current_user.avatar_key).public_url
+    url = file_url(current_user.avatar_key)
     render json: {status: :ok, url: url}
+  end
+
+  def avatar
+    user = User.find(params[:user_id])
+    url = file_url(user.avatar_key)
+    render json: {status: :ok, url: url}   
   end
 
   private
 
   def s3_direct_post
     @resource = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read', content_length_range: 1..(10.megabytes))
+  end
+
+  def file_url(avatar_key)
+    url = Aws::S3::Object.new(ENV['S3_BUCKET_NAME'], avatar_key).public_url    
   end
 
 end
